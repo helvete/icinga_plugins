3ab7d10b476f525c90cd43475413a74e63157593
Martin Peka <martin.peka@argo22.com>
2020-05-04 09:49:13 +0200

    Fix multi metric chart, disable decimals, show total stats

--- templates/check_corona_stats.php
+++ templates/check_corona_stats.php
@@ -7,17 +7,20 @@ $opt[1] = "--title \"Statistics for $servicedesc\" -l 0";
 
 $def[1] = "DEF:$ACT[1]=$RRDFILE[1]:$DS[1]:AVERAGE ";
 $def[1] .= "AREA:$ACT[1]#838383:\"$NAME[1]\t\" ";
-
-$def[1] .= "DEF:$ACT[2]=$RRDFILE[2]:$DS[2]:AVERAGE ";
-$def[1] .= "AREA:$ACT[2]#000000:\"$NAME[2]\t\" ";
-$def[1] .= "GPRINT:$ACT[2]:LAST:\"%2.2lf ".$UNIT[2]." curr\" ";
-$def[1] .= "GPRINT:$ACT[2]:MAX:\"%2.2lf ".$UNIT[2]." max\" ";
-$def[1] .= "GPRINT:$ACT[2]:MIN:\"%2.2lf ".$UNIT[2]." min\\n\" ";
+$def[1] .= "GPRINT:$ACT[1]:LAST:\"%2.0lf ".$UNIT[1]." current\" ";
+$def[1] .= "GPRINT:$ACT[1]:MAX:\"%2.0lf ".$UNIT[1]." max\" ";
+$def[1] .= "GPRINT:$ACT[1]:MIN:\"%2.0lf ".$UNIT[1]." min\\n\" ";
 
 $def[1] .= "DEF:$ACT[3]=$RRDFILE[3]:$DS[3]:AVERAGE ";
 $def[1] .= "AREA:$ACT[3]#99ff99:\"$NAME[3]\t\" ";
-$def[1] .= "GPRINT:$ACT[3]:LAST:\"%2.2lf ".$UNIT[3]." curr\" ";
-$def[1] .= "GPRINT:$ACT[3]:MAX:\"%2.2lf ".$UNIT[3]." max\" ";
-$def[1] .= "GPRINT:$ACT[3]:MIN:\"%2.2lf ".$UNIT[3]." min\\n\" ";
+$def[1] .= "GPRINT:$ACT[3]:LAST:\"%2.0lf ".$UNIT[3]." current\" ";
+$def[1] .= "GPRINT:$ACT[3]:MAX:\"%2.0lf ".$UNIT[3]." max\" ";
+$def[1] .= "GPRINT:$ACT[3]:MIN:\"%2.0lf ".$UNIT[3]." min\\n\" ";
+
+$def[1] .= "DEF:$ACT[2]=$RRDFILE[2]:$DS[2]:AVERAGE ";
+$def[1] .= "AREA:$ACT[2]#000000:\"$NAME[2]\t\" ";
+$def[1] .= "GPRINT:$ACT[2]:LAST:\"%2.0lf ".$UNIT[2]." current\" ";
+$def[1] .= "GPRINT:$ACT[2]:MAX:\"%2.0lf ".$UNIT[2]." max\" ";
+$def[1] .= "GPRINT:$ACT[2]:MIN:\"%2.0lf ".$UNIT[2]." min\\n\" ";
 
 ?>
